e73b480c222a787b9c2416b4e70e60f8309773b1
Harrison <harrison.pinto@d4d.com.co>
2021-07-08 01:24:54 -0500

    range_of, avg_of & size_of have been implemented properly
    Internal functions are exported in order to match the Domain-specific language specifications.
    
    This dictionary makes it easier to create new functionalities into the language itself.
    
    Co-authored-by: Sebastian Garnica <SGman98@users.noreply.github.com>

--- src/utils/internal.py
+++ src/utils/internal.py
@@ -1,8 +1,49 @@
 def transpose(data):
-	return list(map(list, zip(*data['patients'])))
+	return list(map(list, zip(*data)))
 
-def range_of(matrix):
-	import operator
-	a = [ i for (i,j) in sorted(enumerate(matrix), key=operator.itemgetter(1))]
-	# Should equal values be averaged?
-	return [element for _, element in sorted(zip(a, range(1,len(a)+1)))]
+def range_of(data: dict):
+	from operator import itemgetter
+	results = []
+	for matrix in data:
+		a = [ i for (i,j) in sorted(enumerate(matrix), key=itemgetter(1))]
+		result = [element for _, element in sorted(zip(a, range(1,len(a)+1)))]
+
+		current = None
+		i = j = None
+		for r in range(1,len(a)+1):
+			if current is None:
+				current = matrix[result.index(r)]
+				i = j = r
+				continue
+
+			if current == matrix[result.index(r)]:
+				j += 1
+			else:
+				if j - i > 0:
+					val = avg_of([n for n in range(i,j+1)])
+					for n in range(i,j+1):
+						result[result.index(n)] = val
+				current = matrix[result.index(r)]
+				i = j = r
+
+		results.append(result)
+	return results
+
+def avg_of(matrix: list):
+	if type(matrix[0]) is list:
+		return avg_of([avg_of(m) for m in matrix])
+	else:
+		return sum(matrix)/len(matrix)
+
+def size_of(matrix):
+	if type(matrix[0]) is list:
+		return sum([len(m) for m in matrix])
+	else:
+		return len(matrix)
+
+functions = {
+	'range': range_of,
+	'size' : size_of,
+	'avg' : avg_of,
+}
+
